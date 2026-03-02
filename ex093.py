8a501a2c7c3ca6b1c6c6b7356b4362b2cadf1dfd
João Ricardo <ricardojoaofs@hotmail.com>
2026-03-02 12:16:50 -0300

    mini atualização

--- ex093.py
+++ ex093.py
@@ -1,7 +1,7 @@
 jogador = dict()
 jogador['Nome'] = input('Nome do Jogador: ').capitalize()
 jogador['Partidas'] = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
-jogador['Gols'] = 0
+jogador['Gols'] = list()
 for c in range(jogador['Partidas']):
-    jogador['Gols'] = int(input(f'Quantos gols na partida {c}? '))
+    jogador['Gols'] = int(input(f'Quantos gols na {c + 1}o. partida? '))
 print(jogador)
